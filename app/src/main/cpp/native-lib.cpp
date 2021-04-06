bce7d62bf155b8a16fa23e8539f24546f8164b15
Sungki <youbyme@gmail.com>
2021-04-05 22:07:41 -0400

    Added NDK path and log in initProgram()

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,7 @@ extern "C"
 {
     void initProgram()
     {
+        __android_log_print(ANDROID_LOG_VERBOSE, "TestNDK", "initProgram");
     }
 
     void Java_com_example_testndk_Cocos2dxRenderer_nativeInit(JNIEnv*  env, jclass thiz, jint w, jint h)
